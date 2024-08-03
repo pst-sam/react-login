d69549584c1ead3487bb3582202111753e337161
Samuel Afolabi <1samuelafolabi@gmail.com>
2024-08-03 19:15:09 +0100

    added HashRouter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AuthProvider } from './context/AuthProvider';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
@@ -16,13 +16,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <GoogleOAuthProvider clientId='719878321739-fg302079rqtg7j8pidppd6apk5hv2f4u.apps.googleusercontent.com'>
     <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
         <AuthProvider>
           <Routes>
             <Route path='/*' element={<App />} />
           </Routes>
         </AuthProvider>
-      </BrowserRouter>
+      </HashRouter>
     </React.StrictMode>
   </GoogleOAuthProvider>
 );
